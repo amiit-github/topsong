142312834fa7272d3a0b8af0c895d0387fbc569a
sukeshni <=>
2015-05-18 02:13:26 +0900

    sidebar made functional.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express')
 
-// var Itunes = require('./api/Itunes.js')
+var Itunes = require('./api/Itunes.js')
 // var Youtube = require('./api/Youtube.js')
 var client_dir = __dirname + '/public/';
 
 var app = express()
 
 
-//var itunes_rss = new Itunes()
+var itunes_rss = new Itunes()
 //var Youtube = new Youtube();
 
 var results = {}
@@ -21,6 +21,28 @@ app.get('/india/', function(req, res) {
     res.end(JSON.stringify(data));
   });
 })
+
+app.get('/songs', function(req, res) {
+    var country_code = req.query.country;
+    var genre_id = req.query.genreid;
+    var limit = 10;
+    var path = "/"+country_code+"/rss/topsongs/limit="+limit;
+    if (genre_id != "0") path += "/genre="+genre_id;
+    path+="/json";
+
+    if (country_code == "" || genre_id == "") {
+      res.end(JSON.stringify({"msg":"parameter error"}));
+    }
+
+
+    itunes_rss.get(path, function(err, data) {
+      console.log("Requesting path: " + path);
+      console.log("Error : " + err)
+      results.itunes = data
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(data));
+    });
+});
 
 app.get('/', function(req, res) {
     res.sendFile(client_dir + 'index.html');
